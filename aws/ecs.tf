2f9ae3e6390e06ca7da6ba025b2e149379f51f5d
Takashi Kumagai <takashi.kumagai@example.com>
2025-06-11 00:30:27 +0900

    Updated terraform

--- aws/ecs.tf
+++ aws/ecs.tf
@@ -262,6 +262,7 @@ resource "aws_lb_target_group" "main" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+  target_type  = "instance"
 
   health_check {
     path                = "/health"
