23147e53ef44a52ac4614f8fc8f2803e5a7e156a
Takashi Kumagai <takashi.kumagai@example.com>
2025-06-10 23:02:55 +0900

    Added health_check block to aws_lb_target_group

--- aws/ecs.tf
+++ aws/ecs.tf
@@ -262,6 +262,16 @@ resource "aws_lb_target_group" "main" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/health"
+    protocol            = "HTTP"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+    matcher             = "200-299"
+  }
 }
 
 resource "aws_lb_listener" "http" {
